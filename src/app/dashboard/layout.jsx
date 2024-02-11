8cfa4c11e9b55e565652b1df0d737664234b2c5f
geniusahyan <aslamalam078523@gmail.com>
2024-02-12 00:55:28 +0530

    adding dashboard

---
+++ src/app/dashboard/layout.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function DashLayout({children}) {
+  return (
+    <>
+        <h2>this is DashLayout</h2>
+        {children}
+    </>
+  )
+}
+
+export default DashLayout
---
+++ src/app/pages/_app.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function _app() {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default _app
