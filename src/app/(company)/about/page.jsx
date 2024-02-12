af011c610dfb7459d92afff87aaa308f15d48fa5
geniusahyan <aslamalam078523@gmail.com>
2024-02-12 16:53:20 +0530

    usePathname

--- src/app/(company)/about/page.jsx
+++ src/app/(company)/about/page.jsx
@@ -1,5 +1,10 @@
 import React from 'react'
 
+export const metadata = {
+  title: 'about',
+  description: 'about page',
+  keywords: ['about', 'page']
+}
 function Page() {
   return (
     <div className='text-5xl font-bold text-[#7933d4] ' >About</div>
--- src/app/(company)/contact/page.jsx
+++ src/app/(company)/contact/page.jsx
@@ -1,5 +1,10 @@
 import React from 'react'
 
+export const metadata = {
+  title: 'Contact',
+  description: 'Contact page',
+  keywords: ['contact', 'page']
+}
 function Page() {
   return (
     <div className='text-5xl font-bold text-[#5b33ca] ' >Contact</div>
